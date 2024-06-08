9488eb17a14e078f0a5290280324c475cf9323d8
emd1tey <emditey@gmail.com>
2024-06-08 13:00:19 +0200

    test

--- src/utils/cv_generator.py
+++ src/utils/cv_generator.py
@@ -1,15 +1,25 @@
 # src/utils/cv_generator.py
 from fpdf import FPDF
 import markdown
+import os
 from fastapi import FastAPI
 
 async def create_cv(app: FastAPI):
+    # Define paths for markdown, HTML, and PDF files
     markdown_file_path = "src/md/cv.md"
+    html_file_path = "static/cv/index.html"
+    pdf_output_path = "static/cv/resume.pdf"
 
+    # Check if the HTML and PDF files already exist
+    if os.path.exists(html_file_path) and os.path.exists(pdf_output_path):
+        return  # Skip the generation if files already exist
+
+    # Read the markdown content
     with open(markdown_file_path, "r", encoding="utf-8") as file:
         content = file.read()
         html_content = markdown.markdown(content)
 
+    # Define the HTML template
     html_template = f"""
     <!DOCTYPE html>
     <html>
@@ -30,11 +40,12 @@ async def create_cv(app: FastAPI):
     </html>
     """
 
-    with open('static/cv/index.html', 'w') as f:
+    # Write the HTML content to the file
+    with open(html_file_path, 'w') as f:
         f.write(html_template)
 
+    # Create PDF from HTML content
     pdf = FPDF()
     pdf.add_page()
     pdf.write_html(html_content)
-    pdf.output("static/cv/resume.pdf")
-
+    pdf.output(pdf_output_path)
